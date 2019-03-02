be92a0af56749322bd84284ccd7fd0a1a770fd02
Somnath Kayal <somnath.k@flipkart.com>
2019-03-03 00:22:22 +0530

    count distinct substrings

--- coding/ds/string/PatternSearchingTrie.java
+++ coding/ds/string/PatternSearchingTrie.java
@@ -10,6 +10,10 @@ import java.util.Map;
 /*
  * https://www.geeksforgeeks.org/pattern-searching-using-trie-suffixes/
  * search all occurrences of the pattern
+ *
+ * https://www.geeksforgeeks.org/count-distinct-substrings-string-using-suffix-trie/
+ * count distinct substrings
+ * List<Integer> indexes is not required for counting
  */
 public class PatternSearchingTrie {
 
@@ -47,7 +51,6 @@ public class PatternSearchingTrie {
             if(q==null) return null;
             int n=pattern.length()-1;
             if(i==n){
-                System.out.println(q.indexes);
                 for (Integer index : q.indexes){
                     result.add(index-n);
                 }
@@ -63,12 +66,31 @@ public class PatternSearchingTrie {
         for(int i=0;i<text.length();i++){
             insert(text.substring(i),i);
         }
-        System.out.println(root.toString());
         return searchAllOccurrences(pattern);
+    }
+
+    int countDistinctSubstrings(){
+        return countDistinctSubstringsUtil(root);
+    }
+
+    // similar to count nodes in binary tree
+    // this is extended for count nodes in n-ary tree
+    int countDistinctSubstringsUtil(TrieNode current){
+        if(current==null)   return 0;
+        int count=0;
+        for(Map.Entry<Character,TrieNode> trieNodeEntry : current.child.entrySet()){
+            count += countDistinctSubstringsUtil(trieNodeEntry.getValue());
+        }
+        return 1+count;
     }
 
     public static void main(String[] args) {
         PatternSearchingTrie pat=new PatternSearchingTrie();
         System.out.println(pat.search("ababa","abab"));
+        System.out.println(pat.search("ababa","aba"));
+        System.out.println(pat.search("ababa","ab"));
+        System.out.println(pat.search("ababa","ba"));
+
+        System.out.println(pat.countDistinctSubstrings());
     }
 }
